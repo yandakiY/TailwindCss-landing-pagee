7f434c268389db0852a04d7192ba97f59f6adbb3
yandakiY <boayves@gmail.com>
2022-09-25 17:54:30 +0000

    Mise en place Npm TailwindCss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  
+  theme: {
+    extend: {},
+    fontFamily:{
+      Montserrat:['Montserrat', 'sans-serif'],
+      Oswald:['Oswald', 'sans-serif'],
+      Playfair:['Playfair', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
